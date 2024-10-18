aad0e2b56f3d42e19fec2c1715ec11ae8bc3ada7
dukeofgo <formtag0@gmail.com>
2024-10-18 14:07:48 +0700

    add .gitignore & core directories

---
+++ src/main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from . import models
+from .database import engine
+
+
+
+models.Base.metadata.create_all(engine) #used to be (bind=engine)
+
+app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
