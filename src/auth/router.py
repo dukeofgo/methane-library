2a7fbf8556203fc0f91fa67e43bce0a9744530e3
dukeofgo <formtag0@gmail.com>
2024-10-23 23:57:29 +0700

    add token route for logging in

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,6 +1,29 @@
-from fastapi import APIRouter
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+
+from . import constants, schemas, service
+
 
 router = APIRouter(
     prefix = "",
     tags = ["auth"],
 )
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    #authenticate user
+    user = service.authenticate_user(entered_email=form_data.username, entered_password=form_data.password)
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    #create access token
+    access_token_expire_duration = timedelta(days=constants.ACCESS_TOKEN_EXPIRE_DAYS)
+    access_token = service.create_access_token(
+        payload = {"email": form_data.username, "scope": user.status},
+        expires_delta = access_token_expire_duration
+    )
+
+    return schemas.Token(access_token=access_token, token_type="bearer")
